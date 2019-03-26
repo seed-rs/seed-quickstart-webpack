009f14f6aeb33f1581c46bfda851c27a4a00cdcb
Martin Kavík <martin.kavik@abrigo.cz>
2019-03-26 20:08:23 +0100

    rust-webpack-template integrated

---
+++ crate/src/lib.rs
@@ -0,0 +1,39 @@
+use wasm_bindgen::prelude::*;
+
+cfg_if::cfg_if! {
+    // When the `console_error_panic_hook` feature is enabled, we can call the
+    // `set_panic_hook` function to get better error messages if we ever panic.
+    if #[cfg(feature = "console_error_panic_hook")] {
+        use console_error_panic_hook::set_once as set_panic_hook;
+    } else {
+        #[inline]
+        fn set_panic_hook() {}
+    }
+}
+
+cfg_if::cfg_if! {
+    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+    // allocator.
+    if #[cfg(feature = "wee_alloc")] {
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
+}
+
+// Called by our JS entry point to run the example.
+#[wasm_bindgen]
+pub fn run() -> Result<(), JsValue> {
+    set_panic_hook();
+
+    let window = web_sys::window().expect("should have a Window");
+    let document = window.document().expect("should have a Document");
+
+    let p: web_sys::Node = document.create_element("p")?.into();
+    p.set_text_content(Some("Hello from Rust, WebAssembly, and Webpack!!!"));
+
+    let body = document.body().expect("should have a body");
+    let body: &web_sys::Node = body.as_ref();
+    body.append_child(&p)?;
+
+    Ok(())
+}
